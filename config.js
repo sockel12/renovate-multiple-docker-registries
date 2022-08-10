c0dc3f351a4d401ea0b1d90ef4b0d3265225cbec
Benjamin Appel <benjamin.appel@sap.com>
2022-08-10 14:57:32 +0200

    updated config.js

--- config.js
+++ config.js
@@ -1,30 +1,11 @@
 module.exports = {
-    "gitAuthor": "my_username <my_email>",
-    "token": "my_token",
-    "repositories": ["sockel12/renovate-multiple-docker-registries"],
-    //"dryRun": "full",
-    "recreateClosed": true,
-    "packageRules": [
-      {
-        "matchDatasources": [
-          "docker"
-        ],
-        "matchPackagePrefixes": ["quay.io"],
-        "registryUrls": [
-          "https://quay.io/"
-          //"https://my-quay-mirror.registry.com/"
-        ]
-      },
-      {
-        "matchDatasources": [
-          "docker"
-        ],
-        "defaultRegistryUrls": [
-          "https://hub.docker.com/r/"
-          //"https://my-docker-mirror.registry.com/"
-  
-        ]
-      }
-    ]
+  "gitAuthor": "my_username <my_email>",
+  "token": "my_token",
+  "repositories": ["sockel12/renovate-multiple-docker-registries"],
+  //"dryRun": "full",
+  "recreateClosed": true,
+  "registryAliases": {
+    "quay.io": "my-quay-mirror.registry.com",
+    "index.docker.io": "my-docker-mirror.registry.com"
   }
-  
+}
