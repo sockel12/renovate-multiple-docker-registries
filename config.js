fdbec8c3d92294612123102d028fe8b655f7d0da
Benjamin Appel <benjamin.appel@sap.com>
2022-08-12 16:24:36 +0200

    ansible fix

--- config.js
+++ config.js
@@ -1,11 +1,28 @@
 module.exports = {
-  "gitAuthor": "my_username <my_email>",
-  "token": "my_token",
+  "gitAuthor": "myName <myEmail>",
+  "token": "myToken",
   "repositories": ["sockel12/renovate-multiple-docker-registries"],
-  //"dryRun": "full",
+  // "dryRun": "full",
   "recreateClosed": true,
+  "includeForks": true,
   "registryAliases": {
     "quay.io": "my-quay-mirror.registry.com",
     "index.docker.io": "my-docker-mirror.registry.com"
-  }
+  },
+  "kubernetes": {
+    "fileMatch": ["kubernetes-config.yaml"]
+  },
+  "ansible": {
+    "fileMatch":["ansible.yaml"]
+  },
+  "packageRules": [
+    {
+      "matchDatasources": [
+          "docker"
+      ],
+      "defaultRegistryUrls": [
+          "https://my-docker-mirror.registry.com"
+      ]
+    }
+  ]
 }
